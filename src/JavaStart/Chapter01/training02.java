d5cd6e035edf794d305bf240971f08af431335ed
shunpii <shun.s0029@gmail.com>
2022-03-29 23:32:07 +0900

    [finish]1章1-2

--- src/JavaStart/Chapter01/training02.java
+++ src/JavaStart/Chapter01/training02.java
@@ -1,5 +1,22 @@
 package JavaStart.Chapter01;
 
 public class training02 {
+	
+	public static void main(String[] args) {
+		
+		/* 適した方に格納
+		 * ①true
+		 * ②'駆'
+		 * ③3.14
+		 * ④314159265853979L
+		 * ⑤"ミナトの攻撃！的に15ポイントのダメージを与えた"
+		 */
+		
+		boolean flg = true;
+		char run = '駆';
+		double pi = 3.14;
+		long big = 314159265853979L;
+		String attack = "ミナトの攻撃！的に15ポイントのダメージを与えた";
+	}
 
 }
