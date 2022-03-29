76e159a7e8afe73d5865912d12348d3e5c019279
shunpii <shun.s0029@gmail.com>
2022-03-29 22:55:58 +0900

    [finish]1章1-1

--- src/JavaStart/Chapter01/training01.java
+++ src/JavaStart/Chapter01/training01.java
@@ -1,5 +1,18 @@
 package JavaStart.Chapter01;
 
 public class training01 {
+	
+	public static void main(String[] args) {
+		
+		/* 変数aに3,変数bに5を代入
+		 * 変数cに掛け算の結果を代入し出力
+		 */
+		
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		
+		System.out.println("縦幅 " + a + "横幅 " + b + "の長方形の面積は、" + c);
+	}
 
 }
