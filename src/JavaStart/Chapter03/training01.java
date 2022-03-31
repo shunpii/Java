5fc0a9f7209d6c47439367c7c9763c39254d8acc
shunpii <shun.s0029@gmail.com>
2022-03-31 21:57:31 +0900

    [finish]3章1-1

--- src/JavaStart/Chapter03/training01.java
+++ src/JavaStart/Chapter03/training01.java
@@ -1,5 +1,22 @@
 package JavaStart.Chapter03;
 
 public class training01 {
+	
+	public static void main(String[] args) {
+		
+		int isHungry = 1;
+		String food = "もち";
+		
+		System.out.println("こんにちは");
+		if(isHungry == 0) {
+			System.out.println("お腹がいっぱいです");
+		} else {
+			System.out.println("はらぺこです");
+		}
+		if (isHungry == 1) {
+			System.out.println(food + "をいただきます");
+		}
+		System.out.println("ごちそうさまでした");
+	}
 
 }
