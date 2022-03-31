0214e021d83c9f120b738b8903ef4699d2fef88f
shunpii <shun.s0029@gmail.com>
2022-04-01 00:32:38 +0900

    [finish]3章-3

--- src/JavaStart/Chapter03/training03.java
+++ src/JavaStart/Chapter03/training03.java
@@ -1,5 +1,22 @@
 package JavaStart.Chapter03;
 
 public class training03 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("[数あてゲーム]");
+		int ans = new java.util.Random().nextInt(10);
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("0 〜 9の数字を入力してください");
+			int num = new java.util.Scanner(System.in).nextInt();
+			if (num == ans) {
+				System.out.println("アタリ！");
+				break;
+			} else {
+				System.out.println("違います");
+			}
+		}
+		System.out.println("ゲームを終了します");
+	}
 
 }
