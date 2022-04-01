72ceefcff4938b73996820fd454f9aa8777e67ed
shunpii <shun.s0029@gmail.com>
2022-04-01 14:04:03 +0900

    [finish]4章-1

--- src/JavaStart/Chapter04/training01.java
+++ src/JavaStart/Chapter04/training01.java
@@ -1,5 +1,17 @@
 package JavaStart.Chapter04;
 
 public class training01 {
+	
+	public static void main(String[] args) {
+		
+		int[] moneyList = { 121902, 8302, 55100 };
+		for (int i = 0; i < moneyList.length; i++) {
+			System.out.println(moneyList[i]);
+		}
+		
+		for (int num : moneyList) {
+			System.out.println(num);
+		}
+	}
 
 }
