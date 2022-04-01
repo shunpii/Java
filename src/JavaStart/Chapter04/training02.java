ee7d5eb8fd5d3c5a5a596bf702fdb191195be09a
shunpii <shun.s0029@gmail.com>
2022-04-01 14:16:39 +0900

    [finish]4章-2

--- src/JavaStart/Chapter04/training02.java
+++ src/JavaStart/Chapter04/training02.java
@@ -1,5 +1,23 @@
 package JavaStart.Chapter04;
 
 public class training02 {
+	
+	public static void main(String[] args) {
+		
+		int[] numbers = { 3, 4, 9 };
+		System.out.println("1桁の数字を入力してください");
+		int input = new java.util.Scanner(System.in).nextInt();
+//		for (int i = 0; i < numbers.length; i++) {
+//			if (input == numbers[i]) {
+//				System.out.println("アタリ！");
+//			}
+//		}
+		
+		for (int i : numbers) {
+			if (i == input) {
+				System.out.println("アタリ！");
+			}
+		}
+	}
 
 }
