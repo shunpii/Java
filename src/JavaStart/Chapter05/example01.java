8b4ae27c20be9d9ca5b392ee0947fcafe3897703
shunpii <shun.s0029@gmail.com>
2022-04-01 23:17:20 +0900

    [finish]5章例題1

--- src/JavaStart/Chapter05/example01.java
+++ src/JavaStart/Chapter05/example01.java
@@ -1,5 +1,15 @@
 package JavaStart.Chapter05;
 
 public class example01 {
+	
+	public static void main(String[] args) {
+		System.out.println("メソッドを呼び出します");
+		hello();
+		System.out.println("メソッドの呼び出しが終わりました");
+	}
+	
+	public static void hello() {
+		System.out.println("湊さん、こんにちは");
+	}
 
 }
