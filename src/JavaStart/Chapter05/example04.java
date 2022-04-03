b8e57b2f954f1798e3f9ce1e76e30cb0d51c2f05
shunpii <shun.s0029@gmail.com>
2022-04-03 22:22:03 +0900

    [finish]5章例題4

--- src/JavaStart/Chapter05/example04.java
+++ src/JavaStart/Chapter05/example04.java
@@ -1,5 +1,15 @@
 package JavaStart.Chapter05;
 
 public class example04 {
+	
+	public static void main(String[] args) {
+		add(100, 20);
+		add(200, 50);
+	}
+	
+	public static void add(int x, int y) {
+		int ans = x + y;
+		System.out.println(x + "+" + y + "=" + ans);
+	}
 
 }
