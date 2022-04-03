565dda84ef0e5518e63f56b9a233ee9b8d70d0b1
shunpii <shun.s0029@gmail.com>
2022-04-03 22:53:04 +0900

    [finish]5章例題6

---
+++ src/JavaStart/Chapter05/example06.java
@@ -0,0 +1,16 @@
+package JavaStart.Chapter05;
+
+public class example06 {
+	
+	public static void printArray(int[] array) {
+		for (int element : array) {
+			System.out.println(element);
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3 };
+		printArray(array);
+	}
+
+}
