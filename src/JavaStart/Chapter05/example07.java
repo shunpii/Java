d88f8a4b1dce6b6b878e03fb6932c320dd1280f4
shunpii <shun.s0029@gmail.com>
2022-04-04 11:59:11 +0900

    [finish]5章例題7

---
+++ src/JavaStart/Chapter05/example07.java
@@ -0,0 +1,19 @@
+package JavaStart.Chapter05;
+
+public class example07 {
+	
+	public static void incArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			array[i]++;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3 };
+		incArray(array);
+		for (int i : array) {
+			System.out.println(i);
+		}
+	}
+
+}
