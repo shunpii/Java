7dc6fe2c5ba90890bbb06c550a73a72e827eed98
shunpii <shun.s0029@gmail.com>
2022-04-05 23:12:49 +0900

    [finish]練習問題105

---
+++ src/JavaStart/Chapter05/training105.java
@@ -0,0 +1,12 @@
+package JavaStart.Chapter05;
+
+public class training105 {
+	
+	public static void printMessage(String message) {
+		System.out.println(message);
+	}
+	public static void main(String[] args) {
+		printMessage("Hello");
+	}
+
+}
