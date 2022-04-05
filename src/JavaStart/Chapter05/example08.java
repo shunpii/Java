2de9996dc261fcf200885d7e42cb2256a36480b9
shunpii <shun.s0029@gmail.com>
2022-04-05 12:34:06 +0900

    [finish]5章例題8

---
+++ src/JavaStart/Chapter05/example08.java
@@ -0,0 +1,19 @@
+package JavaStart.Chapter05;
+
+public class example08 {
+	
+	public static int[] makeArray(int size) {
+		int[] newArray = new int[size];
+		for (int i = 0; i < newArray.length; i++) {
+			newArray[i] = i;
+		}
+		return newArray;
+	}
+	public static void main(String[] args) {
+		int[] array = makeArray(3);
+		for (int i : array) {
+			System.out.println(i);
+		}
+	}
+
+}
