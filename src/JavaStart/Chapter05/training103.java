d1ba47dd9532c3a7daeb5fd38476905ecbdbbeac
shunpii <shun.s0029@gmail.com>
2022-04-05 18:03:32 +0900

    [finish]練習問題103

---
+++ src/JavaStart/Chapter05/training103.java
@@ -0,0 +1,12 @@
+package JavaStart.Chapter05;
+
+public class training103 {
+	
+	public static void printPI() {
+		System.out.println(Math.PI);
+	}
+	public static void main(String[] args) {
+		printPI();
+	}
+
+}
