4701b8041a95623c1753f35acfc3628a8554f99d
shunpii <shun.s0029@gmail.com>
2022-04-05 15:01:55 +0900

    [finish]5章練習問題3

---
+++ src/JavaStart/Chapter05/training03.java
@@ -0,0 +1,22 @@
+package JavaStart.Chapter05;
+
+public class training03 {
+	
+	public static void main(String[] args) {
+		String title = "こんにちは";
+		String address = "東京";
+		String text = "Hello";
+		email(address, text);
+	}
+	public static void email(String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名；無題");
+		System.out.println("本文；" + text);
+	}
+	public static void email(String title, String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名；" + title);
+		System.out.println("本文；" + text);
+	}
+
+}
