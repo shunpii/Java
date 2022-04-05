f64d451344c1ab52664081e5cd118d4d15610505
shunpii <shun.s0029@gmail.com>
2022-04-05 23:20:33 +0900

    [finish]練習問題106

---
+++ src/JavaStart/Chapter05/training106.java
@@ -0,0 +1,13 @@
+package JavaStart.Chapter05;
+
+public class training106 {
+	
+	public static void printCircleArea(double radius) {
+		double circleArea = radius * radius * 3.14;
+		System.out.println(circleArea);
+	}
+	public static void main(String[] args) {
+		printCircleArea(2.0);
+	}
+
+}
