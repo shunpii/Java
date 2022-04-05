e92151118e9bd3fcdb7370b9b03e849f355d2565
shunpii <shun.s0029@gmail.com>
2022-04-05 14:43:24 +0900

    [finish]5章練習問題1

---
+++ src/JavaStart/Chapter05/training01.java
@@ -0,0 +1,20 @@
+package JavaStart.Chapter05;
+
+public class training01 {
+	
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+	public static void introduceOneself() {
+		String name = "太郎";
+		int age = 20;
+		double height = 170.5;
+		char zodiac = '虎';
+		
+		System.out.println("名前は" + name);
+		System.out.println("歳は" + age);
+		System.out.println("身長は" + height);
+		System.out.println("十二支は" + zodiac);
+	}
+
+}
