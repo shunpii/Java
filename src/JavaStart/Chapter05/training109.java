e2c22e7fdefbf5ab4e9d67619a729c2a0b14591d
shunpii <shun.s0029@gmail.com>
2022-04-06 12:51:16 +0900

    [finish]練習問題109

---
+++ src/JavaStart/Chapter05/training109.java
@@ -0,0 +1,13 @@
+package JavaStart.Chapter05;
+
+public class training109 {
+	
+	public static void printRectangleArea(double height, double width) {
+		double rectangleArea = height * width;
+		System.out.println(rectangleArea + "平方cm");
+	}
+	public static void main(String[] args) {
+		printRectangleArea(10.0, 5.0);
+	}
+
+}
