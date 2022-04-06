ef359fde57bb9c070a7deab0a13840348cacaad3
shunpii <shun.s0029@gmail.com>
2022-04-06 11:48:06 +0900

    [finish]練習問題108

---
+++ src/JavaStart/Chapter05/training108.java
@@ -0,0 +1,14 @@
+package JavaStart.Chapter05;
+
+public class training108 {
+	
+	public static void printMessage(String message, int count) {
+		for (int i = 0; i < count; i++) {
+			System.out.println(message);
+		}
+	}
+	public static void main(String[] args) {
+		printMessage("Hello", 5);
+	}
+
+}
