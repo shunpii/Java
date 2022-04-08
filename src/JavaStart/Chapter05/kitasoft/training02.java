9357314e19879b7df706f5e42c8e1ced4e16304d
shunpii <shun.s0029@gmail.com>
2022-04-08 12:56:51 +0900

    [finish]練習問題02

---
+++ src/JavaStart/Chapter05/kitasoft/training02.java
@@ -0,0 +1,13 @@
+package JavaStart.Chapter05.kitasoft;
+
+public class training02 {
+	
+	public static int average(int x, int y) {
+		return (x + y) / 2;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("2と6の平均は" + average(2, 6));
+	}
+
+}
