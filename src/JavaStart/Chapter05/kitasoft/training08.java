f9fe5115f43670d64107bd72340314ec0bbf04b7
shunpii <shun.s0029@gmail.com>
2022-04-08 15:48:28 +0900

    [finish]練習問題08

---
+++ src/JavaStart/Chapter05/kitasoft/training08.java
@@ -0,0 +1,21 @@
+package JavaStart.Chapter05.kitasoft;
+
+public class training08 {
+	
+	public static int Fibonacci(int n) {
+		if (n == 0) {
+			return 0;
+		} else if (n == 1){
+			return 1;
+		} else {
+			return Fibonacci(n - 2) + Fibonacci(n - 1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		for (int n = 11; n <= 20; n++) {
+			System.out.print(Fibonacci(n) + " ");
+		}
+	}
+
+}
