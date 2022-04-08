0db4a795a8405ca2083f0294d4d1e6362280e2b3
shunpii <shun.s0029@gmail.com>
2022-04-08 12:45:04 +0900

    [finish]練習問題01

---
+++ src/JavaStart/Chapter05/kitasoft/training01.java
@@ -0,0 +1,13 @@
+package JavaStart.Chapter05.kitasoft;
+
+public class training01 {
+	
+	public static int num(int n) {
+		return n * n;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("2の2乗は" + num(2));
+	}
+
+}
