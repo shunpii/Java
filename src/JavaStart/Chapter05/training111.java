d82b8838333327c4da746521ecc151b56ba478c6
shunpii <shun.s0029@gmail.com>
2022-04-08 11:53:50 +0900

    [finish]練習問題111

---
+++ src/JavaStart/Chapter05/training111.java
@@ -0,0 +1,13 @@
+package JavaStart.Chapter05;
+
+public class training111 {
+	
+	public static String getMessage() {
+		return "よろしくおねがいします";
+	}
+	
+	public static void main(String[] args) {
+		String message = getMessage();
+		System.out.println(message);
+	}
+}
