d3141dd443674bed57e218f565f00adde0bd8438
shunpii <shun.s0029@gmail.com>
2022-04-10 14:56:23 +0900

    [finish]6章例題1

---
+++ src/JavaStart/Chapter06/example01/CalcLogic.java
@@ -0,0 +1,12 @@
+package JavaStart.Chapter06.example01;
+
+public class CalcLogic {
+	
+	public static int tasu(int a, int b) {
+		return (a + b);
+	}
+	
+	public static int hiku(int a, int b) {
+		return (a - b);
+	}
+}
