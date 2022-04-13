fad2ba941918d4400113a74fa7d0a7bce20cfbe6
shunpii <shun.s0029@gmail.com>
2022-04-13 12:51:52 +0900

    [finish]6章練習問題1

---
+++ src/JavaStart/Chapter06/training/comment/Zenhan.java
@@ -0,0 +1,13 @@
+package JavaStart.Chapter06.training.comment;
+
+public class Zenhan {
+	
+	public static void doWarusa() {
+		System.out.println("きなこでござる。食えませんがの。");
+	}
+	
+	public static void doTogame() {
+		System.out.println("この老いぼれの目はごまかせませんぞ。");
+	}
+	
+}
---
+++ src/JavaStart/Chapter06/training/main.java
@@ -0,0 +1,14 @@
+package JavaStart.Chapter06.training;
+
+import JavaStart.Chapter06.training.comment.Zenhan;
+
+public class main {
+	
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		JavaStart.Chapter06.training.comment.Kouhan.callDeae();
+		JavaStart.Chapter06.training.comment.Kouhan.showMondokoro();
+	}
+
+}
