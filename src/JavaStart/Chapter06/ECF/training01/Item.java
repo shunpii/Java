b77721be2bfb10c53d0d470309508a6d19eaf97d
shunpii <shun.s0029@gmail.com>
2022-04-14 11:51:16 +0900

    [finish]ECF練習問題1

---
+++ src/JavaStart/Chapter06/ECF/training01/Item.java
@@ -0,0 +1,8 @@
+package JavaStart.Chapter06.ECF.training01;
+
+public class Item {
+	
+	String name;
+	int price;
+
+}
---
+++ src/JavaStart/Chapter06/ECF/training01/Main.java
@@ -0,0 +1,18 @@
+package JavaStart.Chapter06.ECF.training01;
+
+public class Main {
+	
+	public static void main(String[] args) {
+        Item ringo = new Item();
+        Item banana = new Item();
+        ringo.name = "ƒŠƒ“ƒS";
+        ringo.price = 150;
+        banana.name = "ƒoƒiƒi";
+        banana.price = 100;
+        System.out.println(ringo.name);
+        System.out.println(ringo.price);
+        System.out.println(banana.name);
+        System.out.println(banana.price);
+    }
+
+}
