f9abc8c103b6cb266ca4be259e1a70677d18849a
shunpii <shun.s0029@gmail.com>
2022-04-14 13:16:27 +0900

    [finish]ECF練習問題２

---
+++ src/JavaStart/Chapter06/ECF/training02/Main.java
@@ -0,0 +1,14 @@
+package JavaStart.Chapter06.ECF.training02;
+
+public class Main {
+	public static void main(String[] args) {
+        Shop s1 = new Shop();
+        Shop s2 = new Shop();
+        s1.name = "パン屋";
+        s1.sale = 100000;
+        s2.name = "洋服屋";
+        s2.sale = 150000;
+        System.out.println(s1.name + "　売上：" + s1.sale);
+        System.out.println(s2.name + " 売上：" + s2.sale);
+    }
+}
---
+++ src/JavaStart/Chapter06/ECF/training02/Shop.java
@@ -0,0 +1,8 @@
+package JavaStart.Chapter06.ECF.training02;
+
+public class Shop {
+	
+	String name;
+	int sale;
+
+}
