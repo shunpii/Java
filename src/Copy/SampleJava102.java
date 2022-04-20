504f0bf16f9f680fc013b974fdc6d383dad02e91
shunpii <shun.s0029@gmail.com>
2022-04-20 16:56:58 +0900

    [finish]模写102

---
+++ src/Copy/SampleJava102.java
@@ -0,0 +1,14 @@
+package Copy;
+
+public class SampleJava102 {
+	
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for (int i = 1; i <= 5; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+
+}
