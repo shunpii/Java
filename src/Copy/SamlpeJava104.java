a4b7303da9ae7327da3a014900c71a51272ede23
shunpii <shun.s0029@gmail.com>
2022-04-21 13:07:13 +0900

    [finish]模写104

---
+++ src/Copy/SamlpeJava104.java
@@ -0,0 +1,33 @@
+package Copy;
+
+public class SamlpeJava104 {
+	
+	public static void main(String[] args) {
+		int score = new java.util.Random().nextInt(5);
+		String result = null;
+		
+		switch (score += 1) {
+		case 5:
+			result = "A";
+			break;
+		case 4:
+			result = "B";
+			break;
+		case 3:
+			result = "C";
+			break;
+		case 2:
+			result = "D";
+			break;
+		case 1:
+			result = "E";
+			break;
+		default:
+			break;
+		}
+		if (result != null) {
+			System.out.println(result + "ï]âøÇ≈Ç∑ÅB");
+		}
+	}
+
+}
