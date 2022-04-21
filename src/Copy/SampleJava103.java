6eaec259852f985240e5b2eff2e132ce2b1a4e1f
shunpii <shun.s0029@gmail.com>
2022-04-21 12:54:47 +0900

    [finish]模写103

---
+++ src/Copy/SampleJava103.java
@@ -0,0 +1,15 @@
+package Copy;
+
+public class SampleJava103 {
+	
+	public static void main(String[] args) {
+		int a = 11;
+		
+		if (a > 10) {
+			System.out.println("a‚Í10‚æ‚è‘å‚«‚¢‚Å‚·B");
+		} else {
+			System.out.println("a‚Í10ˆÈ‰º‚Å‚·B");
+		}
+	}
+
+}
