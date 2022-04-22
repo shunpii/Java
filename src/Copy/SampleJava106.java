d8e2bbdb408a7b22b3f699360bf4b1d244a4f596
shunpii <shun.s0029@gmail.com>
2022-04-22 17:22:59 +0900

    [finish]模写106

---
+++ src/Copy/SampleJava106.java
@@ -0,0 +1,23 @@
+package Copy;
+
+public class SampleJava106 {
+	
+	public static void main(String[] args) {
+		morning();
+		noon();
+		evening();
+	}
+	
+	public static void morning() {
+		System.out.println("í©Ç≈Ç∑ÅB");
+	}
+	
+	public static void noon() {
+		System.out.println("íãÇ≈Ç∑ÅB");
+	}
+	
+	public static void evening() {
+		System.out.println("ñÈÇ≈Ç∑ÅB");
+	}
+
+}
