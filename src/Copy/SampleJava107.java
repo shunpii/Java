d6e4d5892a8b2e118af25bee05435b8381ed8a26
shunpii <shun.s0029@gmail.com>
2022-04-22 17:42:10 +0900

    [finish]模写107

---
+++ src/Copy/SampleJava107.java
@@ -0,0 +1,14 @@
+package Copy;
+
+public class SampleJava107 {
+	
+	public static void main(String[] args) {
+		int output = plus(10, 30);
+		System.out.println(output);
+	}
+	
+	public static int plus(int a, int b) {
+		return a + b;
+	}
+
+}
