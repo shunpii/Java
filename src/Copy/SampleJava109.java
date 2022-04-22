a81308b1544000c46de59ae055ff0f352c764e92
shunpii <shun.s0029@gmail.com>
2022-04-22 18:11:18 +0900

    [finish]模写109

---
+++ src/Copy/SampleJava109.java
@@ -0,0 +1,16 @@
+package Copy;
+
+public class SampleJava109 {
+	
+	public static void main(String[] args) {
+		String[][] testScore = {
+				{"‘Œê", "80"},
+				{"Z”", "90"},
+				{"Ğ‰ï", "70"},
+				{"—‰È", "75"},
+		};
+		
+		System.out.println("‹³‰È:" + testScore[1][0] + "A“_”:" + testScore[1][1]);
+	}
+
+}
