488b6f421bebb1b6f4eae91d26c048113da9571c
shunpii <shun.s0029@gmail.com>
2022-04-23 19:50:14 +0900

    [finish]模写110

---
+++ src/Copy/SampleJava110.java
@@ -0,0 +1,13 @@
+package Copy;
+
+import java.util.Scanner;
+
+public class SampleJava110 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String inputStr = scan.next();
+		System.out.println("コンソールに入力された文字は、「" + inputStr + "」 です。");
+	}
+
+}
