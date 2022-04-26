fdfa5fe7b00e0294565d055e5db5a5f70e8dfa37
shunpii <shun.s0029@gmail.com>
2022-04-26 19:04:52 +0900

    [finish]模写112

---
+++ src/Copy/SampleJava112.java
@@ -0,0 +1,16 @@
+package Copy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleJava112 {
+	
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("10");
+		list.add("15");
+		list.add("20");
+		System.out.println(list.get(1));
+	}
+
+}
